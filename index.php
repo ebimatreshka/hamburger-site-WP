d75f33f8144602dd2e68d14d415a663556ee391a
Emi <ebimatreshka3@gmail.com>
2022-12-15 00:26:25 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="サイトの説明文が入ります">
+    <title>Hamburger</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1p:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.2.0/css/all.css"> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body class="l-body">
+    <div class="l-container c-grid">
+        <div class="l-container__black"></div>
+
+
+        <!--header-->
+        <header class="l-header p-header c-grid__header c-bg-color--seashell c-flex-box--column">
+            <button class="p-header__title--menu c-font__R c-font-size__20 c-title__brown">Menu
+                <a href="#"></a>
+            </button>
+            <h1 class="p-header__title c-font__R c-titl c-title__brown">Hamburger</h1>
+            <form action="" class="p-search-form fa-search">
+                <label for="" class="p-search-form__screen-reader-text">
+                    <input type="text" class="p-search-form__keyword c-border c-round--63">
+                </label>
+                <label for="" class="p-search-form__screen-reader-text">
+                <input type="button" class="p-search-form__submit c-border c-button--gradation c-round--63 c-font__M--bold" value="検索">
+                </label>
+            </form>
+        </header>
+
+        <!--main visual-->
+        <main class="l-main p-main c-grid__main">
+            <div class="p-main-visual c-background__main">
+                <h1 class="p-main-visual__title c-font__M--bold c-title__white">ダミーサイト</h1>
+            </div>
+
+            <!--menu-->       
+            <article class="p-brunch-menu c-unit--2col">
+                <!-- Take Out -->
+                <ul class="p-brunch-menu__ul-left c-background__menu--left">
+                    <li class="p-brunch-menu__li">
+                        <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">Take Out</h2>
+                        <dl class="p-brunch-menu__dl">
+                            <dt class="p-brunch-menu__dt c-font__M--bold  c-text--dark-brown">Take Out</dt>
+                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                                で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
+                                掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
+                                のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
+                        </dl>
+                        <dl class="p-brunch-menu__dl">
+                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Take Out</dt>
+                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                                で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
+                                掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
+                                のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
+                        </dl>
+                    </li>
+                </ul>
+
+                <!-- Eat In -->
+                <ul class="p-brunch-menu__ul-right c-background__menu--right">
+                    <li class="p-brunch-menuli">
+                        <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">Eat In</h2>
+                        <dl class="p-brunch-menu__dl">
+                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
+                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                        </dl>
+                        <dl class="p-brunch-menu__dl">
+                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
+                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                        </dl>
+                    </li>
+                </ul>
+            </article>
+
+            <!--map-->
+            <section class="p-access c-background__map">
+                <div class="p-access__black"></div>
+                <div class="p-access__container">
+                    <h2 class="p-access__title c-font__M--bold c-title__white c-pseudo-element">見出しが入ります</h2>
+                    <p class="p-access__text c-font-size__14 c-font__M--bold c-text--white">
+                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+                    </p>
+                </div>
+
+            </section>
+        </main>
+
+        <!--sidebar-->
+        <article class="l-sidebar c-grid__sidebar">
+            <nav class="p-nav">
+                <p class="p-nav__hamburger-btn-wrapper">
+                    <a class="p-nav__hamburger-btn-wrapper--btn c-text--brown" href=""></a>
+                </p>
+                <button class="p-nav__title--menu u-margin__title--menu c-text--brown c-font__R">Menu</button>
+                <div>
+                    <h3 class="p-nav__title u-margin__T130 c-font__M--bold">バーガー</h3>
+                    <ul class="p-nav__ul c-font__M--regular LH--195">
+                        <li class="p-nav__li">ハンバーガー</li>
+                        <li class="p-nav__li">チーズバーガー</li>
+                        <li class="p-nav__li">テリヤキバーガー</li>
+                        <li class="p-nav__li">アボガドバーガー</li>
+                        <li class="p-nav__li">フィッシュバーガー</li>
+                        <li class="p-nav__li">ベーコンバーガー</li>
+                        <li class="p-nav__li">チキンバーガー</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 class="p-nav__title u-margin__T11 c-font__M--bold">サイド</h3>
+                    <ul class="p-nav__ul c-font__M--regular LH--195">
+                        <li class="p-nav__li">ポテト</li>
+                        <li class="p-nav__li">サラダ</li>
+                        <li class="p-nav__li">ナゲット</li>
+                        <li class="p-nav__li">コーン</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3 class="p-nav__title u-margin__T18 c-font__M--bold">ドリンク</h3>
+                    <ul class="p-nav__ul c-font__M--regular LH--195">
+                        <li class="p-nav__li">コーラ</li>
+                        <li class="p-nav__li">ファンタ</li>
+                        <li class="p-nav__li">オレンジ</li>
+                        <li class="p-nav__li">アップル</li>
+                        <li class="p-nav__li">紅茶(Ice/Hot)</li>
+                        <li class="p-nav__li">コーヒー(Ice/Hot)</li>
+                    </ul>
+                </div>
+            </nav>
+        </article>
+        
+        <!--footer-->
+        <footer class="l-footer p-footer c-grid__footer c-background__footer">
+            <ul class="p-footer__ul">
+                <li class="p-footer__menu__li c-font__M--regular c-font-size__20">ショップ情報　</li>
+                <li class="p-footer__menu__li c-font__M--regular c-font-size__20">　ヒストリー</li>
+            </ul>
+            <small class="p-footer__copyright c-font__M--regular c-font-size__14">Copyright: RaiseTech</small>
+        </footer>
+        
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+    <script src="scss/js/script.js"></script>
+</body>
+</html>
