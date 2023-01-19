47fbad12ccddd561bfa04647ddfa804d30a7833c
Emi <ebimatreshka3@gmail.com>
2023-01-19 14:44:40 +0900

    wp.scss sidebarのmargin調整

--- searchform.php
+++ searchform.php
@@ -1,10 +1,10 @@
 <!-- getはリクエストするファイルの追加情報がURLパラメータとして付与 -->
 <form class="p-search-form fa-search" method="get" action="<?php echo home_url("/"); ?>">
-<label for="" class="p-search-form__screen-reader-text">
-                    <!-- WPの検索はsというパラメーターで渡されるので"s"を指定 -->
-                    <input type="text" class="p-search-form__keyword c-border c-round--63" name="s" id="s">
-                </label>
-                <label for="" class="p-search-form__screen-reader-text">
-                <input type="button" class="p-search-form__submit c-border c-button--gradation c-round--63 c-font__M--bold" value="検索">
-                </label>
-            </form>
+    <label for="" class="p-search-form__screen-reader-text">
+        <!-- WPの検索はsというパラメーターで渡されるので"s"を指定 -->
+        <input type="text" class="p-search-form__keyword c-border c-round--63" name="s" id="s">
+    </label>
+    <label for="" class="p-search-form__screen-reader-text">
+        <input type="submit" class="p-search-form__submit c-border c-button--gradation c-round--63 c-font__M--bold" value="検索">
+    </label>
+</form>
