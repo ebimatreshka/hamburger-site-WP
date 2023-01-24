d6484db2ecaded2c023ce6331c03c1e13cbfba55
Emi <ebimatreshka3@gmail.com>
2023-01-24 15:59:27 +0900

    jQueryの修正

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 //ハンバーガーメニュー(スライドインさせるために)
+jQuery(function () {
+  $(".p-header__title--menu").click(function () {
+    $(this).addClass("is-active");
+    $(".l-container__black").addClass("is-active"); //黒幕にis-activeをつける(表示)
+    $(".l-sidebar").addClass("is-open"); //sidebarにis-openをつける(表示)
+    $(".l-body").addClass("is-active"); //コンテンツのスクロールバーを非表示に
+    console.log("8cho");
+  });
+});
+
+// 修正用(✖️に効かせる用)
+jQuery(function () {
+  $(".p-nav__hamburger-btn-wrapper").click(function () {
+    console.log("16cho");
+    $(".p-header__title--menu").removeClass("is-active"); //menuの非表示
+    $(".l-sidebar").removeClass("is-open"); //sidebar非表示
+    $(".l-container__black").removeClass("is-active"); //黒幕のis-activeをとる(表示)
+    $(".l-body").removeClass("is-active"); //コンテンツのスクロールバーを表示
+  });
+});
 
 // jQuery(function () {
 //   $(".p-header__title--menu").click(function () {
@@ -16,24 +36,3 @@
 //   $(".l-body").removeClass("is-active"); //コンテンツのスクロールバーを表示
 //   console.log("16cho");
 // });
-
-// 修正用(✖️に効かせる用)
-jQuery(function () {
-  $(".p-header__title--menu").click(function () {
-    $(this).addClass("is-active");
-    $(".l-container__black").addClass("is-active"); //黒幕にis-activeをつける(表示)
-    $(".l-sidebar").addClass("is-open"); //sidebarにis-openをつける(表示)
-    $(".l-body").addClass("is-active"); //コンテンツのスクロールバーを非表示に
-    console.log("8cho");
-  });
-});
-
-jQuery(function () {
-  $(".p-nav__hamburger-btn-wrapper").click(function () {
-    console.log("16cho");
-    $(".p-header__title--menu").removeClass("is-active"); //menuの非表示
-    $(".l-sidebar").removeClass("is-open"); //sidebar非表示
-    $(".l-container__black").removeClass("is-active"); //黒幕のis-activeをとる(表示)
-    $(".l-body").removeClass("is-active"); //コンテンツのスクロールバーを表示
-  });
-});
