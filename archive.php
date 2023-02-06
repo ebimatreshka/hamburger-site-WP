5d7a6b23cf4777262793fd83e9609261bf6852aa
Emi <ebimatreshka3@gmail.com>
2023-02-06 21:02:24 +0900

    archive cardの中身表示と調整

--- archive.php
+++ archive.php
@@ -23,12 +23,14 @@
         <?php while (have_posts()) : the_post(); ?>
             <section class="p-section__card">
                 <figure class="p-section__card__image">
-                    <img class="p-section__card__image--blue" src="<?php echo get_template_directory_uri(); ?>/images/hamburger-card.jpg" alt="バーガー袋に入ったハンバーガーの写真">
+                    <!-- 画像サイズをwidth,heightで指定 -->
+                    <img class="p-section__card__image--blue" src="<?php the_post_thumbnail_url('medium'); ?>" width="407" height="305" alt="バーガー袋に入ったハンバーガーの写真">
                 </figure>
                 <div class="p-section__card__about--wrapper c-bg-color--dark-brown">
                     <h2 class="p-section__card__title c-font__M--bold c-font-size__20 c-color__white"><?php echo get_the_title(); ?></h2>
-                    <h3 class="p-section__card__heading c-font__M--bold c-font-size__16 c-color__white">小見出しが入ります</h3>
-                    <p class="p-section__card__text c-font__M--regular c-font-size__16 c-color__white">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+                    <?php the_content(); ?>
+                    <!-- <h3 class="p-section__card__heading c-font__M--bold c-font-size__16 c-color__white">小見出しが入ります</h3> -->
+                    <!-- <p class="p-section__card__text c-font__M--regular c-font-size__16 c-color__white">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p> -->
                     <button onclick="location.href='<?php the_permalink(); ?>'" class="p-section__card__button c-button__white c-round--35 c-font__M--bold c-font-size__16 c-color__brown c-bg-color--white c-button--white c-LH--106"><a href="<?php the_permalink(); ?>"></a>詳しく見る</button>
                 </div>
             </section>
@@ -60,7 +62,7 @@
         endwhile;
     else :
         ?>
-        <p>お探しのページはありません</p>
+        <p>お探しのページは見つかりませんでした。</p>
     <?php endif; ?>
 
 
