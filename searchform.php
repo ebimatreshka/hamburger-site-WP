b2531a63df675d079d87d0c552348370d3ab3ce5
Emi <ebimatreshka3@gmail.com>
2023-02-09 01:00:28 +0900

    searchformの検索ワード表示

--- searchform.php
+++ searchform.php
@@ -2,7 +2,7 @@
 <form class="p-search-form fa-search" method="get" action="<?php echo home_url("/"); ?>">
     <label for="" class="p-search-form__screen-reader-text">
         <!-- WPの検索はsというパラメーターで渡されるので"s"を指定 -->
-        <input type="text" class="p-search-form__keyword c-border c-round--63" name="s" id="s">
+        <input type="text" class="p-search-form__keyword c-border c-round--63" name="s" id="s" value="<?php the_search_query(); ?>">
     </label>
     <label for="" class="p-search-form__screen-reader-text">
         <input type="submit" class="p-search-form__submit c-border c-button--gradation c-round--63 c-font__M--bold" value="検索">
