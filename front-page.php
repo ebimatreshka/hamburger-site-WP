59855e3729b8601a86a6f3a141c5745648d216b7
Emi <ebimatreshka3@gmail.com>
2023-03-01 00:27:33 +0900

    categoryのcardの編集

--- front-page.php
+++ front-page.php
@@ -1,72 +1,76 @@
 <!-- header.phpを読み込むテンプレートタグ -->
 <?php get_header(); ?>
 
-        <!--main visual-->
-        <main class="l-main p-main c-grid__main">
-            <div class="p-main-visual c-background__main">
-                <h1 class="p-main-visual__title c-font__M--bold c-title__white">ダミーサイト</h1>
-            </div>
+<!--main visual-->
+<main class="l-main p-main c-grid__main">
+    <div class="p-main-visual c-background__main">
+        <h1 class="p-main-visual__title c-font__M--bold c-title__white">ダミーサイト</h1>
+    </div>
 
-            <!--menu-->       
-            <article class="p-brunch-menu c-unit--2col">
-                <!-- Take Out -->
-                <ul class="p-brunch-menu__ul-left c-background__menu--left">
-                    <li class="p-brunch-menu__li">
-                        <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">Take Out</h2>
-                        <dl class="p-brunch-menu__dl">
-                            <dt class="p-brunch-menu__dt c-font__M--bold  c-text--dark-brown">Take Out</dt>
-                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
-                                で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
-                                掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
-                                のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
-                        </dl>
-                        <dl class="p-brunch-menu__dl">
-                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Take Out</dt>
-                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
-                                で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
-                                掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
-                                のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
-                        </dl>
-                    </li>
-                </ul>
+    <!--menu-->
+    <article class="p-brunch-menu c-unit--2col">
+        <!-- Take Out -->
+        <ul class="p-brunch-menu__ul-left c-background__menu--left">
+            <li class="p-brunch-menu__li">
+                <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">
+                    <a href="http://hamburgersite.local/category/takeout/">Take Out</a>
+                </h2>
+                <dl class="p-brunch-menu__dl">
+                    <dt class="p-brunch-menu__dt c-font__M--bold  c-text--dark-brown">Take Out</dt>
+                    <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                        で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
+                        掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
+                        のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
+                </dl>
+                <dl class="p-brunch-menu__dl">
+                    <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Take Out</dt>
+                    <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">当店のテイクアウトで利用できる商品を掲載しています当店のテイクアウト
+                        で利用できる商品を掲載しています当店のテイクアウトで利用できる商品を
+                        掲載しています当店のテイクアウトで利用できる商品を掲載しています当店
+                        のテイクアウトで利用できる商品を掲載しています当店のテイクアウトで</dd>
+                </dl>
+            </li>
+        </ul>
 
-                <!-- Eat In -->
-                <ul class="p-brunch-menu__ul-right c-background__menu--right">
-                    <li class="p-brunch-menuli">
-                        <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">Eat In</h2>
-                        <dl class="p-brunch-menu__dl">
-                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
-                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
-                        </dl>
-                        <dl class="p-brunch-menu__dl">
-                            <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
-                            <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです
-                                店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
-                        </dl>
-                    </li>
-                </ul>
-            </article>
+        <!-- Eat In -->
+        <ul class="p-brunch-menu__ul-right c-background__menu--right">
+            <li class="p-brunch-menuli">
+                <h2 class="p-brunch-menu__title c-title__white c-font__M--bold c-pseudo-element">
+                    <a href="http://hamburgersite.local/category/eatin/">Eat In</a>
+                </h2>
+                <dl class="p-brunch-menu__dl">
+                    <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
+                    <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                </dl>
+                <dl class="p-brunch-menu__dl">
+                    <dt class="p-brunch-menu__dt c-font__M--bold c-text--dark-brown">Eat In</dt>
+                    <dd class="p-brunch-menu__dd c-font__M--regular c-text--dark-brown">店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです
+                        店内でお食事いただけるメニューです店内でお食事いただけるメニューです</dd>
+                </dl>
+            </li>
+        </ul>
+    </article>
 
-            <!--map-->
-            <section class="p-access c-background__map">
-                <div class="p-access__black"></div>
-                <div class="p-access__container">
-                    <h2 class="p-access__title c-font__M--bold c-title__white c-pseudo-element">見出しが入ります</h2>
-                    <p class="p-access__text c-font-size__14 c-font__M--bold c-text--white">
-                        テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
-                    </p>
-                </div>
+    <!--map-->
+    <section class="p-access c-background__map">
+        <div class="p-access__black"></div>
+        <div class="p-access__container">
+            <h2 class="p-access__title c-font__M--bold c-title__white c-pseudo-element">見出しが入ります</h2>
+            <p class="p-access__text c-font-size__14 c-font__M--bold c-text--white">
+                テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。
+            </p>
+        </div>
 
-            </section>
-        </main>
+    </section>
+</main>
 
-        <!--sidebar.phpを読み込むテンプレートタグ-->
-        <?php get_sidebar(); ?>
+<!--sidebar.phpを読み込むテンプレートタグ-->
+<?php get_sidebar(); ?>
 
 <!--footer.phpを読み込むテンプレートタグ -->
 <?php get_footer(); ?>
